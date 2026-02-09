96ebcaddd0c099d09986391111ba3d8ce83404ca
Basmala284 <you@example.com>
2026-02-10 01:56:13 +0200

    first front for events

--- src/app/Student/Activities/page.tsx
+++ src/app/Student/Activities/page.tsx
@@ -1,34 +1,14 @@
 "use client";
 
 import React from "react";
+import Events from "./Events";
+import StaticCard from "./StaticCard";
 
 export default function ActivitiesPage() {
   return (
-    <div style={{ 
-      padding: "40px 20px", 
-      textAlign: "center",
-      minHeight: "60vh",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center"
-    }}>
-      <h1 style={{ 
-        fontSize: "2.5rem", 
-        color: "#2C3A5F",
-        marginBottom: "20px"
-      }}>
-        الأنشطة
-      </h1>
-      <p style={{ 
-        fontSize: "1.2rem", 
-        color: "#64748b",
-        maxWidth: "600px",
-        lineHeight: "1.8"
-      }}>
-        صفحة الأنشطة - قريباً
-      </p>
+    <div style={{ backgroundColor: "#F3F5FD" }}>
+      <StaticCard />
+      <Events />
     </div>
   );
 }
-

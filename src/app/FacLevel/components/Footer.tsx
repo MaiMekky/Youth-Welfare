daf98b4bf80a80c35f7cbb5a8593b69c13a29883
Omar Ahmed <omarsomaa55@gmail.com>
2026-03-07 08:27:28 +0200

    package file to fix fam bug , change footer fac level

--- src/app/FacLevel/components/Footer.tsx
+++ src/app/FacLevel/components/Footer.tsx
@@ -6,16 +6,14 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.left}>
         <p>
-          إدارة التكافل الاجتماعي | جامعة حلوان<br />
-          قسم خدمات الطلاب - نظام إدارة الدعم المالي
+          قسم خدمات الطلاب 
         </p>
       </div>
       <div className={styles.center}>
-        <p>solidarity@helwan.edu.eg :الدعم</p>
-        <p>© 2024 جامعة حلوان. جميع الحقوق محفوظة.</p>
+        <p>© 2026 جامعة العاصمة. جميع الحقوق محفوظة.</p>
       </div>
       <div className={styles.right}>
-        <p>آخر تحديث: ديسمبر 2024</p>
+        <p>آخر تحديث: ديسمبر 2026</p>
         <p>الإصدار 1.0.0 - النظام نشط</p>
       </div>
     </footer>
